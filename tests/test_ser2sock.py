a113b76e2b3494a5d007f5dd3fdf6f9695674d9e
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-07-06 17:49:08 +0200

    More tests

--- tests/test_ser2sock.py
+++ tests/test_ser2sock.py
@@ -1,9 +1,21 @@
 import io
 import os
+import time
+import errno
 import socket
+import threading
 
-from ser2sock import Server, serial, tcp, load_config
+import ser2sock
+from ser2sock import Server, serial, tcp, load_config, main
 
+import pytest
+
+
+CONFIG_TEMPLATE = """
+bridges = [
+    [serial(address="{serial}"), tcp(address=("0", 0))]
+]
+"""
 
 class Hardware:
 
@@ -38,6 +50,28 @@ class Hardware:
 
     def unregister(self, server):
         server.remove_reader(self.master)
+
+
+@pytest.fixture
+def server(tmp_path):
+    with Hardware() as hardware:
+        cfg_filename = tmp_path / 'config.py'
+        with open(cfg_filename, "w") as cfg_file:
+            cfg_file.write(CONFIG_TEMPLATE.format(serial=hardware.serial_name))
+
+        th = threading.Thread(target=ser2sock.main, args=(['-c', cfg_filename],))
+        th.start()
+        while ser2sock.SERVER is None:
+            time.sleep(0.01)
+        server = ser2sock.SERVER
+        server.thread = th
+        server.hardware = hardware
+        hardware.register(server)
+        yield server
+        hardware.unregister(server)
+        server.stop()
+        th.join()
+        os.unlink(cfg_filename)
 
 
 def test_load_config():
@@ -94,6 +128,9 @@ def test_one_bridge():
 
                 while not finished[0]:
                     server.step()
+
+                assert hardware.nb_requests == 1
+
             # internal channel, tcp, master, client (server), client (manual), serial
             assert len(server.listener) == 6
             server.remove_reader(client)
@@ -108,11 +145,42 @@ def test_one_bridge():
         assert not len(server.listener)
 
 
+def test_server(server):
+    _, port = server.bridges[0].sock.getsockname()
+    with socket.create_connection(('localhost', port)) as client:
+        request = b"*IDN?\n"
+        reply = b"ACME,road-runner,v1.245,58477272"
+        client.sendall(request)
+        assert client.recv(1024) == reply
+        assert server.hardware.nb_requests == 1
 
-if __name__ == "__main__":
-    import logging
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s %(levelname)s %(message)s'
-    )
-    test_one_bridge()
+
+def test_server_missing_argument(server):
+    with pytest.raises(SystemExit) as error:
+        ser2sock.main([])
+    assert error.value.code == 2
+
+
+def test_2_clients_to_1_serial(server):
+    _, port = server.bridges[0].sock.getsockname()
+    with socket.create_connection(('localhost', port)) as client1:
+        request = b"*IDN?\n"
+        reply = b"ACME,road-runner,v1.245,58477272"
+        client1.sendall(request)
+        assert client1.recv(1024) == reply
+        assert server.hardware.nb_requests == 1
+
+        with pytest.raises(ConnectionResetError) as error:
+            with socket.create_connection(('localhost', port)) as client2:
+                client2.sendall(b'bla\n')
+                client2.recv(100)
+        assert error.value.errno == errno.ECONNRESET
+        assert server.hardware.nb_requests == 1
+
+    with socket.create_connection(('localhost', port)) as client3:
+        request = b"*IDN?\n"
+        reply = b"ACME,road-runner,v1.245,58477272"
+        client3.sendall(request)
+        assert client3.recv(1024) == reply
+        assert server.hardware.nb_requests == 2
+
